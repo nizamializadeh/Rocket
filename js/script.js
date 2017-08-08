c557c63df43d210a318b870861a4cabae0948eb6
Nizami Alizade <nizamializadeh>
2017-08-08 08:22:21 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,184 @@
+$(window).scroll(function(){
+ 
+        if($(window).scrollTop() >= 2){
+            $(".lamp").css({transform: "translate(0px,"+"19px)"});
+        }else{
+         $(".lamp").css({transform: "translate(0px,"+"-110px)"});
+            
+        }
+    
+});
+$(window).scroll(function(){
+ 
+        if($(window).scrollTop() >= 2){
+            $(".menu").css({transform: "translate(0px,"+"19px)"});
+        }else{
+         $(".menu").css({transform: "translate(0px,"+"-110px)"});
+            
+        }
+    
+});
+$(window).scroll(function(){
+ 
+        if($(window).scrollTop() >= 2){
+            $(".galery_icon").css({transform: "translate(0px,"+"0px)"});
+        }else{
+         $(".galery_icon").css({transform: "translate(0px,"+"299px)"});
+            
+        }
+    
+});
+
+$(window).scroll(function(){
+ 
+        if($(window).scrollTop() >=230){
+           
+            $("#about h1").css("transform", "matrix3d(0.99999,0,-0.00025,0,0,1,0,0,0.00025,0,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            $("#about h1").css("transform", "rotateY(355deg)");
+        }else{
+ 			$("#about h1").css("transform", "matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+        if($(window).scrollTop() >=290){
+           
+            $("#about p").css("transform", "matrix3d(0.99999,0,-0.00925,0,0,1,0,0,0.00025,0,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            // $("#about h1").css("transform", "rotateX(360deg)");
+        }else{
+ 			$("#about p").css("transform", "matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+
+           if($(window).scrollTop() >=330){
+           
+            $("#about h3").css("transform", "matrix3d(0.99999,0,-0.00925,0,0,1,0,0,0.00025,0,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            // $("#about h1").css("transform", "rotateX(360deg)");
+        }else{
+ 			$("#about h3").css("transform", "matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+            if($(window).scrollTop() >=330){
+           
+            $("#about img").css("transform", "matrix3d(0.99999,0,-0.00925,0,0,1,0,0,0.00025,0,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            // $("#about h1").css("transform", "rotateX(360deg)");
+        }else{
+ 			$("#about img").css("transform", "matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+            if($(window).scrollTop() >=330){
+           
+            $("#about h4").css("transform", "matrix3d(0.99999,0,-0.00925,0,0,1,0,0,0.00025,0,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            // $("#about h1").css("transform", "rotateX(360deg)");
+        }else{
+ 			$("#about h4").css("transform", "matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+            if($(window).scrollTop() >=330){
+           
+            $("#about ul").css("transform", "matrix3d(0.99999,0,-0.0025,0,0,1,0,0,0.00025,0,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            // $("#about h1").css("transform", "rotateX(360deg)");
+        }else{
+ 			$("#about ul").css("transform", "matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+
+              if($(window).scrollTop() >=930){
+           
+            $(".slider ").css("transform", "matrix3d(0.99999,0,-0.0025,0,0,1,0,0,0.00025,0,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            // $("#about h1").css("transform", "rotateX(360deg)");
+        }else{
+ 			$(".slider ").css("transform", "matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+               if($(window).scrollTop() >=1200){
+           
+            $("#works .onclick").css("transform", "matrix3d(0.99999,0,-0.0025,0,0,1,0,0,0.00025,0,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            // $("#about h1").css("transform", "rotateX(360deg)");
+        }else{
+ 			$("#works .onclick").css("transform", "matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+                if($(window).scrollTop() >=930){
+           
+            $(".noname ").css("transform", "matrix3d(0.99999,0,-0.00925,0,0,1,0,0,0.00025,0,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            // $("#about h1").css("transform", "rotateX(360deg)");
+        }else{
+ 			$(".noname ").css("transform", "matrix3d(0, 0, 1, 0, 0, -1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+
+            if($(window).scrollTop() >=1200){
+           
+            $("#works h4").css("transform", "matrix3d(0.99999,0,-0.00925,0,0,1,2,0,0.00025,8,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            // $("#about h1").css("transform", "rotateX(360deg)");
+        }else{
+ 			$("#works h4").css("transform", "matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+           if($(window).scrollTop() >=1200){
+           
+            $("#works p").css("transform", "matrix3d(0.99999,0,-0.00925,0,0,1,-2,0,0.00025,8,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            // $("#about h1").css("transform", "rotateX(360deg)");
+        }else{
+ 			$("#works p").css("transform", "matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+
+          if($(window).scrollTop() >=1400){
+           
+            $(".up").css("transform", "matrix3d(0.99999,0,-0.0025,0,0,1,0,0,0.00025,0,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            // $("#about h1").css("transform", "rotateX(360deg)");
+        }else{
+            $(".up").css("transform", "matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+            if($(window).scrollTop() >=1400){
+           
+            $(".two").css("transform", "matrix3d(0.99999,0,-0.0025,0,0,1,-2,0,0.00025,0,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            // $("#about h1").css("transform", "rotateX(360deg)");
+        }else{
+            $(".two").css("transform", "matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+               if($(window).scrollTop() >=1650){
+           
+            $(".two h4").css("transform", "matrix3d(0.99999,0,-0.0025,0,0,1,9,0,0.00025,0,0.99997,0,00002,0,-300,1)");
+            // transform: matrix3d(1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1);
+            // $("#about h1").css("transform", "rotateX(360deg)");
+        }else{
+            $(".two h4").css("transform", "matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -200, 0, -200, 1)");
+            
+        }
+
+
+    
+}); 
+
+        $('.onclick ul li').click(function(){
+        	$(this).$(".slider img").attr("src","img/pic11_m.jpg");
+        });
